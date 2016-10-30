da4e149e4e3c83ce1f9eb610e0c19689e72de4b8
sbojchuk <sbojchuk@gmail.com>
2016-10-30 11:25:48 +0200

    added methods to test csrf

--- src/main/java/com/example/mvc/HomeController.java
+++ src/main/java/com/example/mvc/HomeController.java
@@ -4,7 +4,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -18,5 +21,19 @@ public class HomeController {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("user", authentication);
         return "secret";
+    }
+
+    @ResponseBody
+    @GetMapping("/security")
+    public String securityRequest() {
+        System.out.println("logger");
+        return "Wow, it's do something secret";
+    }
+
+    @ResponseBody
+    @PostMapping("/securityPost")
+    public String securityRequestpost() {
+        System.out.println("logger post");
+        return "Wow, it's do something secret";
     }
 }
